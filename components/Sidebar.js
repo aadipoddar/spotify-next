aa3cfb983245b4b3e708842e097c2c0df018830b
Aadi Poddar <aadipoddarmail@gmail.com>
2022-04-08 01:42:29 +0530

    MiddleWare

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -6,8 +6,12 @@ import {
     HeartIcon,
     RssIcon
 } from '@heroicons/react/outline'
+import { signOut, useSession } from 'next-auth/react'
 
 function Sidebar() {
+
+    const { data: session, status } = useSession();
+
     return (
         <div className='text-gray-500 p-5 text-xs lg:text-sm border-r 
         border-gray-900 overflow-y-scroll scrollbar-hide h-screen sm:max-w-[12rem] lg:max-w-[15rem] 
@@ -15,7 +19,11 @@ function Sidebar() {
 
             <div className='space-y-4'>
 
-                <button className='flex items-center space-x-2 hover:text-white'>
+                <button className='flex items-center space-x-2 hover:text-white' onClick={signOut}>
+                    <p>Log Out</p>
+                </button>
+
+                <button className='flex items-center space-x-2 hover:text-white' onClick={signOut}>
                     <HomeIcon className='h-5 w-5' />
                     <p>Home</p>
                 </button>
--- pages/login.js
+++ pages/login.js
@@ -1,8 +1,14 @@
+import Head from 'next/head'
 import { getProviders, signIn } from 'next-auth/react'
 
 function Login({ providers }) {
     return (
         <div className='flex flex-col items-center bg-black min-h-screen w-full justify-center'>
+
+            <Head>
+                <title>Login</title>
+            </Head>
+
             <img className='w-52 mb-5' src='https://links.papareact.com/9xl' alt='' />
 
             {Object.values(providers).map((provider) => (
